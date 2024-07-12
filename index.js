ed0f6c82803aab35eabf2dd777816e63d5e5ca4f
somilkwatra <somilkwatra21@gmail.com>
2024-07-12 19:03:23 +0530

    server created

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+const app = express();
+app.use(express.json());
+
+const corsOptions = {
+  origin: "*",
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server Running on Port ${PORT}`);
+});
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO, {});
+    console.log("Successfully connected to the database");
+  } catch (err) {
+    console.error("Error connecting to the database:", err);
+    process.exit(1);
+  }
+};
+
+connectDB();
